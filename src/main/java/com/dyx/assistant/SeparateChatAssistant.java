88529ab1eb3989612bbf5e46eabb22aa58e4967c
codeYx <13190416497@163.com>
2026-06-08 16:56:43 +0800

    实战之前

--- src/main/java/com/dyx/assistant/SeparateChatAssistant.java
+++ src/main/java/com/dyx/assistant/SeparateChatAssistant.java
@@ -20,5 +20,7 @@ public interface SeparateChatAssistant {
      * @param userMessage 用户消息
      * @return
      */
+//    @SystemMessage("你是我的好朋友，请用东北话回答问题。")//系统消息提示词
+    @SystemMessage(fromResource = "my-prompt-template.txt")
     String chat(@MemoryId int memoryId, @UserMessage String userMessage);
 }
