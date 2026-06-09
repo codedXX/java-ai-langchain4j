75beff8ee09c943fd7877a1899a25eedb2e3b577
codeYx <13190416497@163.com>
2026-06-09 16:29:43 +0800

    流式输出

--- src/main/java/com/dyx/controller/XiaozhiController.java
+++ src/main/java/com/dyx/controller/XiaozhiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @Tag(name = "硅谷小智")
 @RestController
@@ -19,7 +20,8 @@ public class XiaozhiController {
 
     @Operation(summary = "对话")
     @PostMapping("/chat")
-    public String chat(@RequestBody ChatForm chatForm)  {
+//    public String chat(@RequestBody ChatForm chatForm)  {
+    public Flux<String> chat(@RequestBody ChatForm chatForm)  {
         return xiaozhiAgent.chat(chatForm.getMemoryId(), chatForm.getMessage());
     }
 }
--- src/main/java/com/dyx/pojo/ChatForm.java
+++ src/main/java/com/dyx/pojo/ChatForm.java
@@ -4,6 +4,6 @@ import lombok.Data;
 
 @Data
 public class ChatForm {
-    private int memoryId;//对话id
+    private Long memoryId;//对话id
     private String message;//用户问题
 }
